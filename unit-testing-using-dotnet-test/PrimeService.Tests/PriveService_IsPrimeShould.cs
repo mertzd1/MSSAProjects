2a2c182389996a998498b06a79256c18e7c323ef
DonMertz1 <donald.mertz1@gmail.com>
2020-03-05 11:30:19 -0500

    Upload UnitTest

---
+++ unit-testing-using-dotnet-test/PrimeService.Tests/PriveService_IsPrimeShould.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+using Prime.Services;
+
+namespace Prime.UnitTests.Services
+{
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+       [Theory]
+       [InlineData(4)]
+       [InlineData(9)]
+       [InlineData(12)]
+        public void IsPrime_InputIs1_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(13)]
+        public void IsPrime_InputIs1_ReturnTrue(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.True(result, $"{value} should not be prime");
+        }
+    }
+}
---
+++ unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            for(int i=2; i<candidate; i++)
+            {
+                if (candidate % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
