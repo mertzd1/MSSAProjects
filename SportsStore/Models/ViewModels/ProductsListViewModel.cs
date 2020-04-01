f2657f4e5d51d7a02b253e06815f18a24c8f6cc1
DonMertz1 <donald.mertz1@gmail.com>
2020-04-01 13:51:36 -0400

    SportsStore

---
+++ SportsStore/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SportsStore.Models;
+
+namespace SportsStore.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+
+        public string CurrentCategory { get; set; }
+    }
+}
+    
---
+++ SportsStore/Views/Shared/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent    
+    {
+        private IProductRepository repository;
+        public NavigationMenuViewComponent(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+            return View(repository.Products
+                .Select(x=> x.Category)
+                .Distinct()
+                .OrderBy(x=>x));
+        }
+
+    }
+}
